4fb704d2a7b3738a987db8d7f0748f70fbe5b265
lpadgett <lukepadgett99@gmail.com>
2019-07-13 18:23:24 -0500

    Generated icon images and got splash screen picture

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,3 +1,33 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatefulWidget
+//Widget with mutable state. Constitutes splash screen widget.
+//Hosts current configuration of state
+class SplashScreen extends StatefulWidget {
+
+  //Builds stateful widget. Note that the fat arrow (=>) is a shortcut
+  //for having brackets and a return statement.
+  @override
+  State<StatefulWidget> createState() => new SplashScreenState();
+
+}
+
+//Logic and internal state for splash screen
+//Current configuration of this is hosted in SplashScreen class above
+class SplashScreenState extends State<SplashScreen> {
+
+  //Calls initState()
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  //Concrete implementation of State.build
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration( //Draws a box decoration
+        color: const Color(0xff6320), //Chooses color
+      )
+    )
+  }
+}
