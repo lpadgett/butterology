9adff5b5015b6d7e4c05a5fa602ee3084fe0af2d
lpadgett <lukepadgett99@gmail.com>
2019-07-14 17:19:02 -0500

    Added 'Share Streak' button to home

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,53 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
+//Last Modified: July 2019
+//Author: Luke Padgett
 import 'package:flutter/material.dart';
+import 'analysisScreen.dart';
+import 'strings.dart';
 
-void main() => runApp(ButterologyApp());
+void main() { //Start app
+  runApp(
+      MaterialApp(
+        title: Strings.title,
+        home: HomeScreen()
+      )
+  );
+}
 
-class ButterologyApp extends StatelessWidget {
+//Create Home screen
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: Strings.title,
       home: Scaffold(
+        backgroundColor: Color.fromRGBO(255, 76, 141, 0.9), //Set background color
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text(Strings.home),
         ),
-        body: Center(
-          child: Text('Hello World'),
+        body: Stack ( //Use stack to support aligning multiple children
+          children: <Widget>[
+            Align(
+              alignment: Alignment(0.0, 0.5), //Assign alignment
+              child: FlatButton(
+                child: Text(Strings.analysisButton),
+                color: Color.fromRGBO(252, 245, 85, 0.8), //Set button color
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => AnalysisScreen())); //Navigate to analysis screen
+                }
+              ),
+            ),
+            Align(
+              alignment: Alignment(0.0, 0.7), //Assign alignment
+              child: FlatButton(
+                child: Text(Strings.shareStreakButton),
+                color: Color.fromRGBO(252, 245, 85, 0.8), //Set button color
+                onPressed: () {
+                  //TODO: Create sharing feature so people can share their streaks on social media/text/whatever
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => AnalysisScreen()));
+                }
+              )
+            )
+          ],
         ),
       ),
     );
